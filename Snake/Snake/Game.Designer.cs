b0bc3b76481cc207cccf09630e2e79be50c783d6
YaroslavPuhalskii <netpywka14@gmail.com>
2021-02-18 22:00:12 +0300

    Create new Snake and Create random Fruit

--- Snake/Snake/Game.Designer.cs
+++ Snake/Snake/Game.Designer.cs
@@ -30,36 +30,21 @@ namespace Snake
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.cube = new System.Windows.Forms.PictureBox();
             this.timer = new System.Windows.Forms.Timer(this.components);
-            ((System.ComponentModel.ISupportInitialize)(this.cube)).BeginInit();
             this.SuspendLayout();
-            // 
-            // cube
-            // 
-            this.cube.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.cube.Location = new System.Drawing.Point(0, 0);
-            this.cube.Name = "cube";
-            this.cube.Size = new System.Drawing.Size(40, 40);
-            this.cube.TabIndex = 0;
-            this.cube.TabStop = false;
             // 
             // Game
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.cube);
             this.Name = "Game";
             this.Text = "Game";
-            ((System.ComponentModel.ISupportInitialize)(this.cube)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox cube;
         private System.Windows.Forms.Timer timer;
     }
 }
--- Snake/Snake/Game.cs
+++ Snake/Snake/Game.cs
@@ -12,6 +12,9 @@ namespace Snake
 {
     public partial class Game : Form
     {
+        PictureBox fruit;
+        PictureBox[] snake = new PictureBox[500];
+        private int rI, rJ;
         private int dirX, dirY;
         private int _width = 700;
         private int _height = 600;
@@ -21,14 +24,37 @@ namespace Snake
             InitializeComponent();
             this.Width = _width;
             this.Height = _height;
+            dirX = 1;
+            dirY = 0;
+            fruit = new PictureBox();
+            fruit.BackColor = Color.Green;
+            fruit.Size = new Size(_size, _size);
+            snake[0] = new PictureBox();
+            snake[0].Location = new Point(200, 200);
+            snake[0].Size = new Size(_size, _size);
+            snake[0].BackColor = Color.Blue;
+            this.Controls.Add(snake[0]);
             CreateMap();
+            GenerateFruit();
             timer.Tick += new EventHandler(_update);
-            timer.Interval = 100;
+            timer.Interval = 400;
             timer.Start();
             this.KeyDown += new KeyEventHandler(OKP);
         }
 
 
+        private void GenerateFruit()
+        {
+            Random r = new Random();
+            rI = r.Next(0, _width - _size);
+            int tempI = rI % _size;
+            rI -= tempI;
+            rJ = r.Next(0, _height - _size);
+            int tempJ = rJ % _size;
+            rJ -= tempJ;
+            fruit.Location = new Point(rI, rJ);
+            this.Controls.Add(fruit);
+        }
         private void _update(object sender, EventArgs eventArgs)
         {
             cube.Location = new Point(cube.Location.X + dirX * _size, cube.Location.Y + dirY * _size);
