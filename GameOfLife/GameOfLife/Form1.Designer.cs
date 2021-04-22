05b401e44292748440d92360c7d6f0cc0544a414
YaroslavPuhalskii <netpywka14@gmail.com>
2021-04-22 23:27:17 +0300

    Add life for my pictureBox

--- GameOfLife/GameOfLife/Form1.Designer.cs
+++ GameOfLife/GameOfLife/Form1.Designer.cs
@@ -31,21 +31,21 @@ namespace GameOfLife
         {
             this.components = new System.ComponentModel.Container();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.numResolution = new System.Windows.Forms.NumericUpDown();
-            this.Resolution = new System.Windows.Forms.Label();
-            this.Density = new System.Windows.Forms.Label();
-            this.numDensity = new System.Windows.Forms.NumericUpDown();
             this.bStart = new System.Windows.Forms.Button();
             this.bStop = new System.Windows.Forms.Button();
+            this.Density = new System.Windows.Forms.Label();
+            this.numDensity = new System.Windows.Forms.NumericUpDown();
+            this.Resolution = new System.Windows.Forms.Label();
+            this.numResolution = new System.Windows.Forms.NumericUpDown();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numResolution)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numDensity)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numResolution)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // splitContainer1
@@ -71,41 +71,27 @@ namespace GameOfLife
             this.splitContainer1.SplitterDistance = 150;
             this.splitContainer1.TabIndex = 0;
             // 
-            // pictureBox1
+            // bStart
             // 
-            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(722, 471);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.bStart.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.bStart.Location = new System.Drawing.Point(10, 118);
+            this.bStart.Name = "bStart";
+            this.bStart.Size = new System.Drawing.Size(96, 40);
+            this.bStart.TabIndex = 1;
+            this.bStart.Text = "Start";
+            this.bStart.UseVisualStyleBackColor = true;
+            this.bStart.Click += new System.EventHandler(this.bStart_Click);
             // 
-            // numResolution
+            // bStop
             // 
-            this.numResolution.Location = new System.Drawing.Point(3, 25);
-            this.numResolution.Minimum = new decimal(new int[] {
-            3,
-            0,
-            0,
-            0});
-            this.numResolution.Name = "numResolution";
-            this.numResolution.Size = new System.Drawing.Size(120, 23);
-            this.numResolution.TabIndex = 1;
-            this.numResolution.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
-            this.numResolution.Value = new decimal(new int[] {
-            3,
-            0,
-            0,
-            0});
-            // 
-            // Resolution
-            // 
-            this.Resolution.AutoSize = true;
-            this.Resolution.Location = new System.Drawing.Point(3, 7);
-            this.Resolution.Name = "Resolution";
-            this.Resolution.Size = new System.Drawing.Size(63, 15);
-            this.Resolution.TabIndex = 2;
-            this.Resolution.Text = "Resolution";
+            this.bStop.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.bStop.Location = new System.Drawing.Point(10, 164);
+            this.bStop.Name = "bStop";
+            this.bStop.Size = new System.Drawing.Size(96, 43);
+            this.bStop.TabIndex = 2;
+            this.bStop.Text = "Stop";
+            this.bStop.UseVisualStyleBackColor = true;
+            this.bStop.Click += new System.EventHandler(this.bStop_Click);
             // 
             // Density
             // 
@@ -134,27 +120,41 @@ namespace GameOfLife
             0,
             0});
             // 
-            // bStart
+            // Resolution
             // 
-            this.bStart.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.bStart.Location = new System.Drawing.Point(10, 118);
-            this.bStart.Name = "bStart";
-            this.bStart.Size = new System.Drawing.Size(96, 40);
-            this.bStart.TabIndex = 1;
-            this.bStart.Text = "Start";
-            this.bStart.UseVisualStyleBackColor = true;
-            this.bStart.Click += new System.EventHandler(this.bStart_Click);
+            this.Resolution.AutoSize = true;
+            this.Resolution.Location = new System.Drawing.Point(3, 7);
+            this.Resolution.Name = "Resolution";
+            this.Resolution.Size = new System.Drawing.Size(63, 15);
+            this.Resolution.TabIndex = 2;
+            this.Resolution.Text = "Resolution";
             // 
-            // bStop
+            // numResolution
             // 
-            this.bStop.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.bStop.Location = new System.Drawing.Point(10, 164);
-            this.bStop.Name = "bStop";
-            this.bStop.Size = new System.Drawing.Size(96, 43);
-            this.bStop.TabIndex = 2;
-            this.bStop.Text = "Stop";
-            this.bStop.UseVisualStyleBackColor = true;
-            this.bStop.Click += new System.EventHandler(this.bStop_Click);
+            this.numResolution.Location = new System.Drawing.Point(3, 25);
+            this.numResolution.Minimum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numResolution.Name = "numResolution";
+            this.numResolution.Size = new System.Drawing.Size(120, 23);
+            this.numResolution.TabIndex = 1;
+            this.numResolution.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.numResolution.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(722, 471);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
             // timer1
             // 
@@ -169,14 +169,15 @@ namespace GameOfLife
             this.Controls.Add(this.splitContainer1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel1.PerformLayout();
             this.splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numResolution)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numDensity)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numResolution)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
--- GameOfLife/GameOfLife/Form1.cs
+++ GameOfLife/GameOfLife/Form1.cs
@@ -35,23 +35,77 @@ namespace GameOfLife
             cols = pictureBox1.Width / resolution;
             field = new bool[cols, rows];
 
+            Random rnd = new Random();
+            for (int x = 0; x < cols; x++)
+            {
+                for (int y = 0; y < rows; y++)
+                {
+                    field[x, y] = rnd.Next((int)numDensity.Value) == 0;
+                }
+            }
+
             pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
             graphics = Graphics.FromImage(pictureBox1.Image);
+            timer1.Start();
         }
 
         private void bStart_Click(object sender, EventArgs e)
         {
+            pictureBox1.BackColor = Color.Black;
             StartGame();
+        }
+
+        private void StopGame()
+        {
+            if (!timer1.Enabled)
+                return;
+
+            timer1.Stop();
+            numResolution.Enabled = true;
+            numDensity.Enabled = true;
         }
 
         private void bStop_Click(object sender, EventArgs e)
         {
+            StopGame();
+        }
 
+        private int CountNeighbours(int x, int y)
+        {
+            return 0;
+        }
+
+        private void NextGeneretion()
+        {
+            graphics.Clear(Color.Black);
+            var newfield = new bool[cols, rows];
+
+            for (int x = 0; x < cols; x++)
+            {
+                for (int y = 0; y < rows; y++)
+                {
+                    var neighboursCount = CountNeighbours(x, y);
+                    var haslife = field[x, y];
+
+                    if (!haslife && neighboursCount == 3)
+                        newfield[x, y] = true;
+                    else if (haslife && neighboursCount != 3)
+                        newfield[x, y] = false;
+                    else
+                        newfield[x, y] = field[x, y];
+
+                    if (field[x, y])
+                        graphics.FillRectangle(Brushes.Red, x * resolution, y * resolution, resolution, resolution);
+
+                }
+            }
+            field = newfield;
+            pictureBox1.Refresh();
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-
+            NextGeneretion();
         }
     }
 }
