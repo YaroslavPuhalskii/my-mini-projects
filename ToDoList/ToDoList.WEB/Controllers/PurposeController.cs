66dfa0ab16fc28a23064d37b0de31ab9db668bfa
yaroslav <netpywka14@gmail.com>
2021-09-18 12:20:24 +0300

    fix

--- ToDoList/ToDoList.WEB/Controllers/PurposeController.cs
+++ ToDoList/ToDoList.WEB/Controllers/PurposeController.cs
@@ -1,73 +1,131 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Threading.Tasks;
 using System.Web.Mvc;
+using ToDoList.Core;
+using ToDoList.Entities.Models;
+using ToDoList.WEB.Models;
 
 namespace ToDoList.WEB.Controllers
 {
     public class PurposeController : Controller
     {
-        // GET: Purpose
-        public ActionResult Index()
+        private readonly IPurposeRepo purposeRepo;
+
+        public PurposeController(IPurposeRepo repo)
+        {
+            purposeRepo = repo;
+        }
+
+        public ViewResult Index()
         {
             return View();
         }
 
-        // GET: Purpose/Details/5
-        public ActionResult Details(int id)
+        public async Task<PartialViewResult> Load()
         {
-            return View();
-        }
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<Purpose, PurposeIndexView>());
+            var map = new Mapper(config);
+            var purposes = map.Map<List<PurposeIndexView>>(await purposeRepo.GetItems());
 
-        // GET: Purpose/Create
-        public ActionResult Create()
-        {
-            return View();
+            return PartialView(purposes);
         }
-
-        // POST: Purpose/Create
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        [HttpGet]
+        public async Task<PartialViewResult> Details(int id)
         {
             try
             {
-                // TODO: Add insert logic here
+                var config = new MapperConfiguration(cfg => cfg.CreateMap<Purpose, PurposeIndexView>());
+                var map = new Mapper(config);
+                var purpose = map.Map<Purpose, PurposeIndexView>(await purposeRepo.GetById(id));
 
-                return RedirectToAction("Index");
+                return PartialView(purpose);
             }
             catch
             {
-                return View();
+                return PartialView("~/Views/Shared/Error.cshtml");
             }
         }
 
-        // GET: Purpose/Edit/5
-        public ActionResult Edit(int id)
+        public PartialViewResult Create()
         {
-            return View();
+            return PartialView();
         }
 
-        // POST: Purpose/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public async Task<JsonResult> Create(PurposeCreateView item)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var config = new MapperConfiguration(cfg => cfg.CreateMap<PurposeCreateView, Purpose>());
+                    var map = new Mapper(config);
+                    var purpose = map.Map<PurposeCreateView, Purpose>(item);
+
+                    await purposeRepo.Insert(purpose);
+
+                    return Json(new { result = true });
+                }
+                catch (Exception ex)
+                {
+                    return Json(new { result = false, message = ex.Message });
+                }
+            }
+
+            return Json(new { result = false, message = "Model is invalid!" });
+        }
+
+        [HttpGet]
+        public async Task<PartialViewResult> Edit(int id)
         {
             try
             {
-                // TODO: Add update logic here
+                var config = new MapperConfiguration(cfg => cfg.CreateMap<Purpose, PurposeEditView>());
+                var map = new Mapper(config);
+                var purpose = map.Map<Purpose, PurposeEditView>(await purposeRepo.GetById(id));
 
-                return RedirectToAction("Index");
+                return PartialView(purpose);
             }
             catch
             {
-                return View();
+                return PartialView("~/Views/Shared/Error.cshtml");
             }
         }
 
-        // GET: Purpose/Delete/5
-        public ActionResult Delete(int id)
+        [HttpPost]
+        public async Task<JsonResult> Edit(PurposeEditView item)
         {
-            return View();
+            try
+            {
+                var config = new MapperConfiguration(cfg => cfg.CreateMap<PurposeEditView, Purpose>());
+                var map = new Mapper(config);
+                var purpose = map.Map<PurposeEditView, Purpose>(item);
+
+                await purposeRepo.Update(purpose);
+
+                return Json(new { result = true});
+            }
+            catch (Exception ex)
+            {
+                return Json(new { result = false, message = ex.Message});
+            }
+        }
+
+        [HttpGet]
+        public async Task<JsonResult> Delete(int id)
+        {
+            try
+            {
+                await purposeRepo.Remove(id);
+
+                return Json(new { result = true });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { result = false, message = ex.Message });
+            }
         }
 
         // POST: Purpose/Delete/5
---
+++ ToDoList/ToDoList.WEB/Models/PurposeModelView.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ToDoList.WEB.Models
+{
+    public class PurposeIndexView
+    {
+        [HiddenInput(DisplayValue = false)]
+        public int PurposeId { get; set; }
+        [Required]
+        [Display(Name = "Название цели")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "Описание")]
+        public string Description { get; set; }
+    }
+
+    public class PurposeEditView
+    {
+        [HiddenInput(DisplayValue = false)]
+        public int PurposeId { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "Название должно содержать хотя бы один символ!")]
+        [Display(Name = "Название цели")]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "Описание должно содержать хотя бы один символ!")]
+        [Display(Name = "Описание")]
+        public string Description { get; set; }
+    }
+
+    public class PurposeCreateView
+    {
+        [HiddenInput(DisplayValue = false)]
+        public int PurposeId { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "Название должно содержать хотя бы один символ!")]
+        [Display(Name = "Название цели")]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "Описание должно содержать хотя бы один символ!")]
+        [Display(Name = "Описание")]
+        public string Description { get; set; }
+    }
+}
